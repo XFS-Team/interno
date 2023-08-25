198fc508d416dfc219ff0b780898b3f82901e044
thaolv <thaolv.dev@gmail.com>
2023-08-25 10:01:42 +0700

    feat: homepage

---
+++ src/components/Homepage/Banner.svelte
@@ -0,0 +1,47 @@
+<script>
+  
+</script>
+
+<div class="relative">
+  <img src="/images/banner-img.png" alt="bannerImage" />
+  <div class="absolute top-[208px] left-[52px]">
+    <div class="">
+      <p class="text-[65px] not-italic font-[600] leading-[125%] max-w-[460px]">
+        Let Your Home Be Unique
+      </p>
+      <p
+        class="mt-[18px] text-[22px] font-[400] text-[#4D5053] leading-[150%] tracking-[0.22px] max-w-[460px]"
+      >
+        There are many variations of the passages of lorem Ipsum fromavailable,
+        majority.
+      </p>
+      <p
+        class="mt-[18px] text-[22px] font-[400] text-[#4D5053] leading-[150%] tracking-[0.22px] max-w-[460px]"
+      >
+        There are many variations of the passages of lorem Ipsum fromavailable,
+        majority.
+      </p>
+    </div>
+    <div class="">
+      <button
+        class="mt-[21px] py-[26px] pl-[48px] pr-[49px] flex items-center gap-[10px] rounded-[18px] bg-[#292F36] text-[18px] font-[600] text-[#fff] leading-[125%] tracking-[0.36px]"
+        >Get Started
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="19"
+          height="17"
+          viewBox="0 0 19 17"
+          fill="none"
+        >
+          <path
+            d="M2 8.43542L15.7232 8.29857M10.6182 1.91138L17.1412 8.43436L10.4868 15.0887"
+            stroke="#CDA274"
+            stroke-width="2"
+            stroke-linecap="square"
+            stroke-linejoin="round"
+          />
+        </svg>
+      </button>
+    </div>
+  </div>
+</div>
---
+++ src/components/Homepage/Work.svelte
@@ -0,0 +1,94 @@
+<script>
+</script>
+
+<div class="my-[200px] mx-auto flex justify-center items-center gap-[42px]">
+  <div class="max-w-[358px] flex flex-col items-center justify-between">
+    <div class="text-[25px] font-[400] leading-[125%]">Project Plan</div>
+    <div
+      class="mt-[20px] text-[22px] text-center font-[400] leading-[150%] tracking-[0.22px]"
+    >
+      There are many variations of the passages of lorem Ipsum from available,
+      majority.
+    </div>
+    <div
+      class="cursor-pointer flex items-center gap-[12px] mt-[52px] text-[18px] font-[600] leading-[125%] tracking-[0.36px]"
+    >
+      Read More
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="19"
+        height="17"
+        viewBox="0 0 19 17"
+        fill="none"
+      >
+        <path
+          d="M1.92969 8.43542L15.6529 8.29857M10.5479 1.91138L17.0708 8.43436L10.4165 15.0887"
+          stroke="#CDA274"
+          stroke-width="2"
+          stroke-linecap="square"
+          stroke-linejoin="round"
+        />
+      </svg>
+    </div>
+  </div>
+
+  <div class="max-w-[358px] flex flex-col items-center justify-between">
+    <div class="text-[25px] font-[400] leading-[125%]">Interior Work</div>
+    <div
+      class="mt-[20px] text-[22px] text-center font-[400] leading-[150%] tracking-[0.22px]"
+    >
+      There are many variations of the passages of lorem Ipsum from available,
+      majority.
+    </div>
+    <div
+      class="cursor-pointer flex items-center gap-[12px] mt-[52px] text-[18px] font-[600] leading-[125%] tracking-[0.36px]"
+    >
+      Read More
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="19"
+        height="17"
+        viewBox="0 0 19 17"
+        fill="none"
+      >
+        <path
+          d="M1.92969 8.43542L15.6529 8.29857M10.5479 1.91138L17.0708 8.43436L10.4165 15.0887"
+          stroke="#CDA274"
+          stroke-width="2"
+          stroke-linecap="square"
+          stroke-linejoin="round"
+        />
+      </svg>
+    </div>
+  </div>
+
+  <div class="max-w-[358px] flex flex-col items-center justify-between">
+    <div class="text-[25px] font-[400] leading-[125%]">Realization</div>
+    <div
+      class="mt-[20px] text-[22px] text-center font-[400] leading-[150%] tracking-[0.22px]"
+    >
+      There are many variations of the passages of lorem Ipsum from available,
+      majority.
+    </div>
+    <div
+      class="cursor-pointer flex items-center gap-[12px] mt-[52px] text-[18px] font-[600] leading-[125%] tracking-[0.36px]"
+    >
+      Read More
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="19"
+        height="17"
+        viewBox="0 0 19 17"
+        fill="none"
+      >
+        <path
+          d="M1.92969 8.43542L15.6529 8.29857M10.5479 1.91138L17.0708 8.43436L10.4165 15.0887"
+          stroke="#CDA274"
+          stroke-width="2"
+          stroke-linecap="square"
+          stroke-linejoin="round"
+        />
+      </svg>
+    </div>
+  </div>
+</div>
