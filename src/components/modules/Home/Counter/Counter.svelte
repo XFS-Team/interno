af1eec248ed5084d651743893ac343f33e8ffacf
thaolv <thaolv.dev@gmail.com>
2023-08-26 22:14:26 +0700

    feat: homepage

---
+++ src/components/modules/Home/Counter/Counter.svelte
@@ -0,0 +1,46 @@
+<script>
+  import CounterItem from "./CounterItem.svelte";
+
+  let counter1 = {
+    number: 12,
+    content: "Years Of Experiance",
+  };
+  let counter2 = {
+    number: 85,
+    content: "Success Project",
+  };
+  let counter3 = {
+    number: 15,
+    content: "Active Project",
+  };
+  let counter4 = {
+    number: 95,
+    content: "Happy Customers",
+  };
+</script>
+
+<div class="h-[457px] my-[200px] bg-[#F4F0EC]">
+  <div class="max-w-[1200px] h-[100%] mx-auto">
+    <div class="h-[100%] flex justify-center items-center">
+      <CounterItem number={counter1.number} content={counter1.content} />
+      <div class="w-[1px] h-[130px] bg-[#CDA274]" />
+      <CounterItem
+        class="pl-[76px]"
+        number={counter2.number}
+        content={counter2.content}
+      />
+      <div class="w-[1px] h-[130px] bg-[#CDA274]" />
+      <CounterItem
+        class="pl-[76px]"
+        number={counter3.number}
+        content={counter3.content}
+      />
+      <div class="w-[1px] h-[130px] bg-[#CDA274]" />
+      <CounterItem
+        class="pl-[76px]"
+        number={counter4.number}
+        content={counter4.content}
+      />
+    </div>
+  </div>
+</div>
---
+++ src/components/modules/Home/Work/Work.svelte
@@ -0,0 +1,10 @@
+<script>
+  import WorkContent from "./WorkItem.svelte";
+  let titles = ["Project Plan", "Interior Work", "Realization"];
+</script>
+
+<div class="container my-[200px] flex justify-center items-center gap-[42px]">
+  {#each titles as title}
+    <WorkContent {title} />
+  {/each}
+</div>
