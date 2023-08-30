5298272c824ddf4002eb221ddcd47f790fa85ec2
thaolv <thaolv.dev@gmail.com>
2023-08-30 22:25:25 +0700

    feat: blog

---
+++ src/components/modules/BlogPage/Banner/Banner.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import Banner from "../../Banner/Banner.svelte";
+
+  const banner = {
+    image: "/images/BlogPage/Banner/banner.png",
+    title: "Articles & News",
+    pageName: "Home / Blog",
+    class: "py-[41px] px-[78px]"
+  };
+</script>
+
+<Banner {...banner} />
