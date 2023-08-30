f3b832685f133c35dbffe4f63522d7eb58266426
thaolv <thaolv.dev@gmail.com>
2023-08-30 22:25:25 +0700

    feat: blog

---
+++ src/components/modules/BlogPage/LatestPost/LatestPost.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import Text from "../../../common/Text/Text.svelte";
+  import Title from "../../../common/Title/Title.svelte";
+</script>
+
+<div class="container">
+  <Title class="text-[50px] tracking-[1px] mb-[27px]">Latest Post</Title>
+  <div
+    class="p-[22px] flex items-center gap-[65px] rounded-[62px] border-[1px] border-solid border-[#E7E7E7] shadow-[0_10px_30px_0_rgba(255,255,255,0.25)]"
+  >
+    <img src="/images/BlogPage/LatestPost/latest_post.png" alt="latestPost" />
+    <div class="max-w-[469px]">
+      <Title class="text-[25px] tracking-[0.5px] mb-[22px]">Low Cost Latest Invented Interior Designing Ideas</Title>
+      <Text class="mb-[22px]"
+        >Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae turpis
+        dignissim maximus.posuere in.Contrary to popular belief.</Text
+      >
+      <Text
+        >orem Ipsum is not simply random text. It has roots in a piece of
+        classica.</Text
+      >
+      <div class="flex items-center justify-between mt-[41px]">
+        <Text>26 December,2022</Text>
+        <img src="/images/arrow_button_default.svg" alt="arrowButton" />
+      </div>
+    </div>
+  </div>
+</div>
---
+++ src/components/modules/BlogPage/News/News.svelte
@@ -0,0 +1,68 @@
+<script lang="ts">
+  import Title from "../../../common/Title/Title.svelte";
+  import BlogItem from "../../Blog/BlogItem.svelte";
+
+  const blogs = [
+    {
+      image: "/images/Blog/blog_1.png",
+      tag: "Kitchan Design",
+      content: "Let’s Get Solution For Building Construction Work",
+      date: "26 December,2022",
+      colorArrow: "default",
+    },
+    {
+      image: "/images/Blog/blog_2.png",
+      tag: "Living Design ",
+      content: "Low Cost Latest Invented Interior Designing Ideas.",
+      date: "22 December,2022",
+      colorArrow: "white",
+      class: "bg-[#F4F0EC]",
+    },
+    {
+      image: "/images/Blog/blog_3.png",
+      tag: "Interior Design",
+      content: "Best For Any Office & Business Interior Solution",
+      date: "25 December,2022",
+      colorArrow: "default",
+    }, 
+  ];
+
+  const blogs2 = [
+        {
+      image: "/images/Blog/blog_4.png",
+      tag: "Kitchan Design",
+      content: "Let’s Get Solution For Building Construction Work",
+      date: "26 December,2022",
+      colorArrow: "default",
+    },
+    {
+      image: "/images/Blog/blog_5.png",
+      tag: "Living Design ",
+      content: "Low Cost Latest Invented Interior Designing Ideas.",
+      date: "22 December,2022",
+      colorArrow: "white",
+    },
+    {
+      image: "/images/Blog/blog_6.png",
+      tag: "Interior Design",
+      content: "Best For Any Office & Business Interior Solution",
+      date: "25 December,2022",
+      colorArrow: "default",
+    }, 
+  ]
+</script>
+
+<div class="container mt-[150px]">
+  <Title class="text-[50px] tracking-[1px]">Articles & News</Title>
+  <div class="mt-[30px] flex justify-between items-center gap-[28px]">
+    {#each blogs as blog}
+      <BlogItem {...blog} />
+    {/each}
+  </div>
+
+  <div class="mt-[30px] flex justify-between items-center gap-[28px]">
+    {#each blogs2 as blog}
+      <BlogItem {...blog} />
+    {/each}
+  </div>
+</div>
