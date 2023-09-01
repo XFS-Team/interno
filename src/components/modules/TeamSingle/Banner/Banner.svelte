5584fa9d514eb8d5e2b4e4ece1b13a970d345c17
thaolv <thaolv.dev@gmail.com>
2023-09-01 22:30:07 +0700

    feat: team-single

---
+++ src/components/modules/TeamSingle/Banner/Banner.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import Banner from "../../Banner/Banner.svelte";
+
+  const banner = {
+    image: "/images/TeamSingle/Banner/banner.png",
+    title: "Professional Single",
+    pageName: "Home / Team Single",
+    class: "py-[41px] px-[65px]",
+  };
+</script>
+
+<Banner {...banner} />
+ 
