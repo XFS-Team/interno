4ac0b4bcdf2c686c160d64ec9ae2219b3741f802
thaolv <thaolv.dev@gmail.com>
2023-09-02 18:26:57 +0700

    feat: contact-us

---
+++ src/components/modules/ContactUs/Banner/Banner.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import Banner from "../../Banner/Banner.svelte";
+
+  const banner = {
+    image: "/images/ContactUs/Banner/banner.png",
+    title: "Contact Us",
+    pageName: "Home / Contact",
+    class: "py-[41px] px-[130px]",
+  };
+</script>
+
+<Banner {...banner} />
