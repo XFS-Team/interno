13d3a13c62ba5259767e4c1fbdb8cc2a3d070bef
thaolv <thaolv.dev@gmail.com>
2023-09-02 18:26:57 +0700

    feat: contact-us

---
+++ src/components/modules/ContactUs/ContactForm/ContactForm.svelte
@@ -0,0 +1,79 @@
+<script lang="ts">
+  import Button from "../../../common/Button/Button.svelte";
+  import Text from "../../../common/Text/Text.svelte";
+  import Title from "../../../common/Title/Title.svelte";
+  import Input from "../../BlogDetails/Form/Input.svelte";
+
+  const contactInfors = {
+    mailImg: "/images/ContactUs/Icons/mail.svg",
+    mail: "info@yourdomain.com",
+    phoneImg: "/images/ContactUs/Icons/phone.svg",
+    phone: "+1 (378) 400-1234",
+    websiteImg: "/images/ContactUs/Icons/earth.svg",
+    website: "www.yourdomain.com",
+    socials: ["facebook", "twitter", "linkedin", "instagram"],
+  };
+
+  const inputs = ["Name", "Email", "Website", "Phone"];
+  const textareaName = "Hello Iam Intrested in..";
+</script>
+
+<div class="container flex flex-col items-center mt-[200px]">
+  <Title
+    class="w-[683px] text-[50px] leading-[150%] tracking-[0.5px] text-center"
+    >We love meeting new people and helping them.</Title
+  >
+  <div class="flex items-start justify-between gap-[55px] mt-20">
+    <div class="w-[349px] py-[76px] px-8 bg-pale-cream rounded-[50px]">
+      <div class="w-[285px] flex flex-col items-start gap-y-[30px]">
+        <div class="flex items-center">
+          <img src={contactInfors.mailImg} alt="mailImg" />
+          <Text class="text-xl leading-[150%] tracking-[0.2px] ml-[15px]"
+            >{contactInfors.mail}</Text
+          >
+        </div>
+        <div class="flex items-center">
+          <img src={contactInfors.phoneImg} alt="phoneImg" />
+          <Text class="text-xl leading-[150%] tracking-[0.2px] ml-[15px]"
+            >{contactInfors.phone}</Text
+          >
+        </div>
+        <div class="flex items-center">
+          <img src={contactInfors.websiteImg} alt="websiteImg" />
+          <Text class="text-xl leading-[150%] tracking-[0.2px] ml-[15px]"
+            >{contactInfors.website}</Text
+          >
+        </div>
+      </div>
+
+      <div class="flex items-center gap-[35px] mt-[47px]">
+        {#each contactInfors.socials as social}
+          <img
+            class="h-[21px]"
+            src="/images/Socials/{social}.svg"
+            alt="socialImg"
+          />
+        {/each}
+      </div>
+    </div>
+
+    <div class="flex flex-col items-end">
+      <div
+        class="flex flex-wrap items-center justify-between gap-y-[50px]"
+      >
+        {#each inputs as input}
+          <Input name={input} />
+        {/each}
+        <textarea
+          class="w-full outline-none text-[22px] text-slate leading-[150%] tracking-[0.22px] border-b-[1px] border-solid border-[#000] h-[175px]"
+          placeholder={textareaName}
+        />
+      </div>
+
+      <Button class="mt-[58px] py-[26px] px-[54px] flex items-center gap-2.5">
+        <Text intent="secondary" color="secondary">Send Now</Text>
+        <img src="/images/arrow_right.svg" alt="arrowRight" />
+      </Button>
+    </div>
+  </div>
+</div>
---
+++ src/components/modules/ContactUs/Map/Map.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+</script>
+
+<div class="container mt-[150px] mb-[200px] relative">
+  <img class="rounded-[70px]" src="/images/ContactUs/Map/map.png" alt="map" />
+  <img
+    class="absolute top-[150px] left-[325px]"
+    src="/images/ContactUs/Icons/location.svg"
+    alt="location"
+  />
+</div>
