52cbc2c8b3e8ad4db713f0b63fb0058ac931c582
thaolv <thaolv.dev@gmail.com>
2023-09-05 00:26:41 +0700

    feat: 404, password, changelog page

---
+++ src/components/modules/Changelog/Banner/Banner.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import Banner from "../../Banner/Banner.svelte";
+
+  const banner = {
+    image: "/images/Changelog/Banner/banner.png",
+    title: "Changelog",
+    pageName: "Home / Changelog",
+    class: "py-[41px] px-[136px]",
+  };
+</script>
+
+<Banner {...banner} />
---
+++ src/components/modules/Password/Password/Password.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import Button from "../../../common/Button/Button.svelte";
+  import Text from "../../../common/Text/Text.svelte";
+  import Title from "../../../common/Title/Title.svelte";
+  import Input from "../../BlogDetails/Form/Input.svelte";
+
+  const inputName = "Enter Your Password";
+</script>
+
+<div
+  class="container my-[200px] flex flex-col justify-center items-center py-[154px] px-60 bg-pale-cream rounded-[70px]"
+>
+  <Title class="text-[50px] leading-[150%] tracking-[0.5px]"
+    >Password Protected</Title
+  >
+  <Text class="w-[718px] leading-[150%] tracking-[0.22px] text-center"
+    >This page is password protected. If you are the website admin, or have
+    access to this page, please type your password below.</Text
+  >
+  <div class="flex flex-col items-center mt-[50px]">
+    <Input class="bg-pale-cream placeholder-dark-gray" name={inputName} />
+    <Button class="mt-[41px] py-[26px] px-[54px] flex items-center gap-[9px]">
+      <Text intent="secondary" color="secondary">Submit Now</Text>
+      <img src="/images/arrow_right.svg" alt="arrowRight" />
+    </Button>
+  </div>
+</div>
