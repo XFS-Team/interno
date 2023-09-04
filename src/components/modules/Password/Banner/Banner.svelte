4794b24a82941d1ab2541d6d33bf7c1c12309873
thaolv <thaolv.dev@gmail.com>
2023-09-05 00:26:41 +0700

    feat: 404, password, changelog page

---
+++ src/components/modules/Password/Banner/Banner.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import Banner from "../../Banner/Banner.svelte";
+
+  const banner = {
+    image: "/images/Password/Banner/banner.png",
+    title: "Restricted Page",
+    pageName: "Home / Restricted Page",
+    class: "py-[41px] px-[75px]",
+  };
+</script>
+
+<Banner {...banner} />
