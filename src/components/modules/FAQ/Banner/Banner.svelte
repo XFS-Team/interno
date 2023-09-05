3f302bd55edf64808c0452c51118dc885318e1a8
thaolv <thaolv.dev@gmail.com>
2023-09-05 22:55:52 +0700

    feat: faq

---
+++ src/components/modules/FAQ/Banner/Banner.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import Banner from "../../Banner/Banner.svelte";
+
+  const banner = {
+    image: "/images/FAQ/Banner/banner.png",
+    title: "Faq’s",
+    pageName: "Home / Faq’s",
+    class: "py-[41px] px-[180px]",
+  };
+</script>
+
+<Banner {...banner} />
