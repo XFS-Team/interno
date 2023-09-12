6d2985081e958001dbaff32774936b1864c38e6c
thaolv <thaolv.dev@gmail.com>
2023-09-12 09:53:23 +0700

    feat: license

---
+++ src/components/modules/License/Banner/Banner.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import Banner from "../../Banner/Banner.svelte";
+
+  const banner = {
+    image: "/images/License/Banner/banner.png",
+    title: "Licenses",
+    pageName: "Home / Licenses",
+    class: "py-[41px] px-[157px]",
+  };
+</script>
+
+<Banner {...banner} />
